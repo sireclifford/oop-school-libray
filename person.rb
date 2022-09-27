c2ada7c3d5ec48b79315d7ebc4ea8ed881a223e1
sireclifford <dev.sire.clifford@gmail.com>
2022-09-27 22:12:01 +0000

    add linters

--- person.rb
+++ person.rb
@@ -1,27 +1,18 @@
+# Person class
 class Person
-    def initialize(name = "Unknown", age, parent_permission=true)
-        @id = id
-        @name = name
-        @age = age
-    end
+  def initialize(age, name = 'Unknown', _parent_permission: true)
+    @id = id
+    @name = name
+    @age = age
+  end
 
-    def id
-        @id
-    end
+  attr_reader :id, :name, :age
 
-    def name
-        @name
-    end
+  def can_use_services?
+    @parent_permission || is_of_age?
+  end
 
-    def age
-        @age
-    end
-    def can_use_services?
-        @parent_permission || is_of_age?
-    end
-
-    def is_of_age?
-        @age >= 18
-    end
-
+  def of_age?
+    @age >= 18
+  end
 end
--- student.rb
+++ student.rb
@@ -1,14 +1,12 @@
 require_relative 'person'
-
+# Student class
 class Student < Person
-    
-    def initialize(classroom)
-        super(name, age, parent_permission)
-        @classroom = classroom
-    end
+  def initialize(classroom)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
 
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
-  
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
