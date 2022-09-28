c437dd504746ba666123e997b9214ca4c10d8b7d
sireclifford <dev.sire.clifford@gmail.com>
2022-09-28 06:26:22 +0000

    modify person class methods

--- person.rb
+++ person.rb
@@ -1,16 +1,18 @@
 # Person class
 class Person
+  attr_accessor :name, :age
+
   def initialize(age, name = 'Unknown', _parent_permission: true)
     @id = id
     @name = name
     @age = age
   end
 
-  attr_reader :id, :name, :age
-
   def can_use_services?
     @parent_permission || is_of_age?
   end
+
+  private
 
   def of_age?
     @age >= 18
