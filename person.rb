79702abe5ba6157a94700fc4c84da398e4249fe8
sireclifford <dev.sire.clifford@gmail.com>
2022-09-28 21:56:18 +0000

    create and implement decorators

--- person.rb
+++ person.rb
@@ -1,9 +1,10 @@
+require_relative 'nameable'
 # Person class
 class Person
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', _parent_permission: true)
-    @id = id
+    @id = Random.rand(1...1000)
     @name = name
     @age = age
   end
@@ -12,9 +13,22 @@ class Person
     @parent_permission || is_of_age?
   end
 
+  def correct_name
+    @name
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+
+capitalized_trimmed_person = TrimmerDecorator.new(person)
+puts capitalized_trimmed_person.correct_name
