64c48c1a205b540db637937e96362b355ca48a5c
sireclifford <dev.sire.clifford@gmail.com>
2022-09-29 10:29:53 +0000

    Set up associations between classes and objects.

--- student.rb
+++ student.rb
@@ -1,6 +1,8 @@
 require_relative 'person'
 # Student class
 class Student < Person
+  attr_reader :classroom
+
   def initialize(classroom)
     super(name, age, parent_permission)
     @classroom = classroom
@@ -8,5 +10,10 @@ class Student < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 end
