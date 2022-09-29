a65984e49c6d1e15cc3bb637b0ac900bc9946c25
sireclifford <dev.sire.clifford@gmail.com>
2022-09-29 09:05:48 +0000

    inherit Nameable in Person

--- person.rb
+++ person.rb
@@ -1,12 +1,15 @@
 require_relative 'nameable'
 # Person class
-class Person
-  attr_accessor :name, :age
+class Person < Nameable
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
 
   def initialize(age, name = 'Unknown', _parent_permission: true)
     @id = Random.rand(1...1000)
     @name = name
     @age = age
+    @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
@@ -17,18 +20,13 @@ class Person
     @name
   end
 
+  def add_rental(date, book)
+    Rental.new(date, book, self)
+  end
+
   private
 
   def of_age?
     @age >= 18
   end
 end
-
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-
-capitalized_trimmed_person = TrimmerDecorator.new(person)
-puts capitalized_trimmed_person.correct_name
